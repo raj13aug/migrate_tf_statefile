0c1289f1acf9d5b7b3789344330cd2e61f752123
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-07-20 12:47:47 +0530

    move to folder

--- TF/sa.tf
+++ TF/sa.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "gcs" {
-    credentials = "terraform_credentails.json"
-    bucket      = "gke-terraform-tfstate-bucket-demo-1" # GCS bucket name to store terraform tfstate
-    prefix      = "gke-terraform-cluster"               # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     credentials = "terraform_credentails.json"
+#     bucket      = "gke-terraform-tfstate-bucket-demo-1" # GCS bucket name to store terraform tfstate
+#     prefix      = "gke-terraform-cluster"               # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
+#   }
+# }
 
 module "service-accounts" {
   source     = "terraform-google-modules/service-accounts/google"
